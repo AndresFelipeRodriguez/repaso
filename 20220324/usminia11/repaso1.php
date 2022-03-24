fe80b1eb15c69ff54cfd6686f5c9af6d143d689a
AndresFelipeRodriguez <101894151+AndresFelipeRodriguez@users.noreply.github.com>
2022-03-24 17:29:57 -0500

    Add files via upload
    repaso de php conectando documentos y datos para que salgan los datos en otra pagina conectando las dos paginas de codigo

---
+++ 20220324/usminia11/repaso1.php
@@ -0,0 +1,29 @@
+<html>
+<head> </head>
+
+<body>
+<h1> hola soy un titulo</h1>
+
+
+<?php
+//inicia codigo php
+//esto es un comentario
+/*todo lo que este aqui se considera un comentario  */
+
+//comando hecho devuelve un valor
+
+echo"hola esto es un comentario";
+echo "<br>";  //salto de linea
+echo 3+3; /* realiza una operacion en php
+             que no se puede en html, */
+
+//termina php
+
+
+?>
+
+
+
+
+</body>
+</html>
---
+++ 20220324/usminia11/variable.php
@@ -0,0 +1,12 @@
+<?php
+//definimos una variable con un texto 
+$miVariable = "hola  variable";
+//visualizamos la variable en el explorador 
+echo $miVariable;
+//otro ejemplo miValor igual a 2
+echo"<br>";
+$miValor=5;
+//visualizamos sim embaego realizamos una multiplicacion * 3
+echo 3*$miValor;
+
+?>
